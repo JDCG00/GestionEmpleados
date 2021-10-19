50be4c130e773c49b221641cfdf6bf2ae7cebb3a
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-10-19 23:34:18 +0200

    v1

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!--
-    <?php
+<?php
     function formulario1(){
         echo '
             <h3>¿Qué desea hacer: CONSULTAR datos / dar de ALTA / dar de BAJA datos / MODIFICAR datos ?</h3>
@@ -10,7 +9,6 @@
         ';
     }
 ?>
--->
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -18,10 +16,8 @@
     <title>Gestión Empleados</title>
 </head>
 <body>
-    <h3>¿Qué desea hacer: CONSULTAR datos / dar de ALTA / dar de BAJA datos / MODIFICAR datos ?</h3>
-    <p><a href="php/consultar.php">1.- CONSULTAR DATOS</a></p>
-    <p><a href="php/alta.php">2.- DAR DE ALTA DATOS</a></p>
-    <p><a href="php/baja.php">3.- DAR DE BAJA DATOS</a></p>
-    <p><a href="php/modificar.php">4.- MODIFICAR DATOS</a></p>
+    <?php
+        formulario1();
+    ?>
 </body>
 </html>

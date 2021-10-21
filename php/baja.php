8ea0c4bd9a500635df9dd507c6d4b2d595a68e6e
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-10-22 00:11:51 +0200

    v1

--- php/baja.php
+++ php/baja.php
@@ -15,15 +15,24 @@
             <label for="baja">Usuario que desea eliminar: </label>
             <select>
                 <?php
-                    $consulta = "SELECT * FROM empleados ";
+                    $consulta = "SELECT idEmpleados, Nombre, DNI FROM empleados ";
                     $resultado = $conexion->query($consulta);
                     while ($fila = $resultado->fetch_assoc()) {
-                        
+                        $id = $fila['idEmpleados'];
+                        $nombre = $fila['Nombre'];
+                        $dni = $fila['DNI'];
+                        echo "<option value=$id>$nombre con DNI: $dni</option>";
                     }
                 ?>
             </select>
             <input type="submit" name="borrar" value="Borrar"><br><br>
+            <?php
+                if ($_POST) {
+                    $consulta2 = "DELETE FROM empleados WHERE idEmpleados = '".$id."'";
+                    $resultado2 = $conexion->query($consulta2);
+                }
+            ?>
             <a href="../index.php"><input type="button" name="volver" value="Volver"></a>
-    </form>
+    </form>   
 </body>
 </html>

73dde3df1225173dbfb871fadf10f7018c50a782
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-10-22 13:01:41 +0200

    v1

---
+++ php/modificar.php
@@ -0,0 +1,38 @@
+<?php
+    require("conexion.php");
+    $conexion = new mysqli(SERVIDOR, USUARIO, PW, BD);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Modificar</title>
+</head>
+<body>
+    <h1>Modificar</h1>
+        <form action="modificar.php" method="post">
+            <label for="modificar">Usuario que desea eliminar: </label>
+            <select>
+                <?php
+                    $consulta = "SELECT idEmpleados, Nombre, DNI FROM empleados ";
+                    $resultado = $conexion->query($consulta);
+                    while ($fila = $resultado->fetch_assoc()) {
+                        $id = $fila['idEmpleados'];
+                        $nombre = $fila['Nombre'];
+                        $dni = $fila['DNI'];
+                        echo "<option value=$id>$nombre con DNI: $dni</option>";
+                    }
+                ?>
+            </select>
+            <input type="submit" name="borrar" value="Borrar"><br><br>
+            <?php
+                if ($_POST) {
+                    $consulta2 = "DELETE FROM empleados WHERE idEmpleados = '".$id."'";
+                    $resultado2 = $conexion->query($consulta2);
+                }
+            ?>
+            <a href="../index.php"><input type="button" name="volver" value="Volver"></a>
+    </form>   
+</body>
+</html>
